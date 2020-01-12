ee0424c48b715d2886f96172400b4e15b2092acf
Zoranzoki21 <zorandori4444@gmail.com>
2020-01-12 17:30:51 +0100

    Convert PopcornEditor to use extension registration
    Bug: T242540
    Change-Id: I246a38b9c20acffbd85d98b85a84cd078c59feac

--- PopcornEditor.php
+++ PopcornEditor.php
@@ -5,75 +5,20 @@
  *
  * Currently using a lot of base code form old SVGEdit extension.
  *
- * @copyright 2010-2016 Brion Vibber <brion@pobox.com>
+ * @copyright 2010-2020 Brion Vibber <brion@pobox.com>
  *
  * MediaWiki-side code is GPL
  */
-
-$wgExtensionCredits['other'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'PopcornEditor',
-	'author'         => array( 'Brion Vibber' ),
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:PopcornEditor',
-	'descriptionmsg' => 'popcorn-desc',
-);
-
-$wgMessagesDirs['PopcornEditor'] = __DIR__ . '/i18n';
-
-$wgHooks['BeforePageDisplay'][] = 'PopcornEditorHooks::beforePageDisplay';
-$wgHooks['MakeGlobalVariablesScript'][] = 'PopcornEditorHooks::makeGlobalVariablesScript';
-
-$wgAutoloadClasses['PopcornEditorHooks'] = dirname( __FILE__ ) . '/PopcornEditor.hooks.php';
-
-$myResourceTemplate = array(
-	'localBasePath' => dirname( __FILE__ ) . '/modules',
-	'remoteExtPath' => 'PopcornEditor/modules',
-	'group' => 'ext.popcorn',
-);
-$wgResourceModules += array(
-	'ext.popcorn.editor' => $myResourceTemplate + array(
-		'scripts' => array(
-			'ext.popcorn.embedapi.js',
-			'ext.popcorn.formmultipart.js',
-			'ext.popcorn.io.js',
-			'ext.popcorn.editor.js',
-		),
-		'styles' => array(
-			'ext.popcorn.editButton.css',
-		),
-		'messages' => array(
-			'popcorn-summary-label',
-			'popcorn-summary-default',
-			'popcorn-editor-save-close',
-			'popcorn-editor-close',
-		),
-		'dependencies' => array(
-			'jquery.ui'
-		)
-	),
-);
-$wgResourceModules += array(
-	'ext.popcorn.editButton' => $myResourceTemplate + array(
-		'scripts' => array(
-			'ext.popcorn.editButton.js',
-		),
-		'messages' => array(
-			'popcorn-editbutton-edit',
-			'popcorn-edit-tab',
-			'popcorn-edit-tab-tooltip'
-		),
-		'dependencies' => array(
-			'ext.popcorn.editor'
-		)
-	),
-);
-
-// Set this to point to editor.html
-//
-$wgPopcornEditorUrl = 'http://popcorn-editor.wmflabs.org/PopcornEditor/editor.html';
-
-// Allow saving the popcorn JSON files
-$wgFileExtensions[] = 'popcorn';
-
-// @todo?
-//$wgMediaHandlers['application/popcorn+json'] = 'PopcornEditorHandler';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'PopcornEditor' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['PopcornEditor'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the PopcornEditor extension. ' .
+		'Please use wfLoadExtension() instead, ' .
+		'see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the PopcornEditor extension requires MediaWiki 1.29+' );
+}
